85cf3c1aa6d5656a12608464b1b67629fcd3057f
Nathaniel Brion Sison <ngsison@outlook.com>
2023-01-14 19:33:31 +0800

    Initial Commit

---
+++ NateApp/NateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NateAppApp.swift
+//  NateApp
+//
+//  Created by Nathaniel Brion Sison on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
