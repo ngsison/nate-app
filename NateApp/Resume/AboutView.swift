062923d18a3b8644b10a814db60bbc7c67b0443d
Nathaniel Brion Sison <ngsison@outlook.com>
2023-01-21 03:12:09 +0800

    Add experiences

--- NateApp/Resume/AboutView.swift
+++ NateApp/Resume/AboutView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import StepperView
 
 struct AboutView: View {
     @Environment(\.colorScheme) var colorScheme
@@ -25,8 +26,9 @@ struct AboutView: View {
                 VStack(alignment: .leading, spacing: 20) {
                     Header()
                     Skills()
+                    Experiences()
                 }
-                .padding(.horizontal, 40)
+                .padding(.horizontal, 32)
                 .padding(.vertical)
                 .navigationTitle("About Me")
             }
@@ -126,5 +128,56 @@ fileprivate struct SkillView: View {
         .frame(width: 70, height: 70)
         .cornerRadius(10)
         .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 4)
+    }
+}
+
+fileprivate struct Experiences: View {
+    let steps = [Experience(jobTitle: "Freelance iOS Developer", companyName: "Upwork", year: "May 2021 - Present"),
+                 Experience(jobTitle: "Lead iOS Engineer", companyName: "GCash", year: "Dec 2018 - June 2021"),
+                 Experience(jobTitle: "Lead Mobile App Developer", companyName: "TAP Services, Inc.", year: "2017-2018"),
+                 Experience(jobTitle: "Junior .NET Developer", companyName: "Smart Communications, Inc.", year: "2016 - 2017"),
+                 Experience(jobTitle: "Student Partner", companyName: "Microsoft", year: "2015 - 2016")]
+    
+    let indicationTypes = [StepperIndicationType.custom(NumberedCircleView(text: "4")),
+                           .custom(NumberedCircleView(text: "3")),
+                           .custom(NumberedCircleView(text: "2")),
+                           .custom(NumberedCircleView(text: "1")),
+                           .custom(NumberedCircleView(text: "-"))]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Experience")
+                .font(.headline)
+                .padding(.top)
+            
+            StepperView()
+                .addSteps(steps)
+                .indicators(indicationTypes)
+                .stepIndicatorMode(.vertical)
+                .spacing(30)
+                .lineOptions(.custom(1, .gray))
+                .stepperEdgeInsets(EdgeInsets(.zero))
+        }
+    }
+}
+
+fileprivate struct Experience: View {
+    var jobTitle: String
+    var companyName: String
+    var year: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(jobTitle)
+                .font(.system(size: 16, weight: .medium))
+            
+            Text(companyName)
+                .font(.system(size: 16))
+            
+            Text(year)
+                .font(.system(size: 16))
+                .foregroundColor(.secondary)
+            
+        }
     }
 }
