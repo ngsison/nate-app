0e52f54643359339741db160ced58bea26a9e9ff
Nathaniel Brion Sison <ngsison@outlook.com>
2023-01-20 22:08:37 +0800

    Add GitHubView

---
+++ NateApp/Features/Networking/GitHubSampleAPI/GitHubView.swift
@@ -0,0 +1,20 @@
+//
+//  GitHubView.swift
+//  NateApp
+//
+//  Created by Nathaniel Brion Sison on 1/19/23.
+//
+
+import SwiftUI
+
+struct GitHubView: View {
+    var body: some View {
+        Text("github")
+    }
+}
+
+struct GitHubPreview: PreviewProvider {
+    static var previews: some View {
+        GitHubView()
+    }
+}
